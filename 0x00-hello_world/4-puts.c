18c1daa9e8013a7cbb9bdb799668ffb72793b890
mrbridge-dev <oseijuantuah@gmail.com>
2022-06-04 09:05:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	const char *words = "\"Programming is like building a multilingual puzzle";
+
 	puts(words);
 	return (0);
 }
