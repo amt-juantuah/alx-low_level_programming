c08796617a5924a52100ed4ce06d0f40b4dc8bfb
mrbridge-dev <oseijuantuah@gmail.com>
2022-06-04 14:35:49 -0700

    alph in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase in reverse
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	for (; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
