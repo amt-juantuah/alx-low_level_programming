6d7a1ce700b1fa5cf7a88e1e407eb03726c5c115
mrbridge-dev <oseijuantuah@gmail.com>
2022-06-28 09:36:48 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,10 @@ char *create_array(unsigned int size, char c)
 		{
 			return NULL;
 		}
-		array[i] = c;
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
 		return (array);
 	}
 	return NULL;
