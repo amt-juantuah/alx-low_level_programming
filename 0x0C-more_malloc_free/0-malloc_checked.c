ddb99fcc19292a9b0118d08d3e37ba8558fc806b
mrbridge-dev <oseijuantuah@gmail.com>
2022-07-03 17:32:34 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int argument
+ * Return: None
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *tmp = NULL;
+
+	tmp = malloc(sizeof(b));
+	if (tmp == NULL)
+	{
+		exit(98);
+	}
+	return (tmp);
+}
