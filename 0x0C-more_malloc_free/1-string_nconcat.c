a6f7e0027fae4a31d6ea0a70fe1ab04d643cfee6
mrbridge-dev <oseijuantuah@gmail.com>
2022-07-03 19:21:15 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,9 +26,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	tmp = malloc(j + n);
 	if (tmp == NULL)
 		return (NULL);
-	for (i = 0; i <= j; i++)
+	for (i = 0; i < j; i++)
 		tmp[i] = s1[i];
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		tmp[n + i] = s2[i];
 	return (tmp);
 }
