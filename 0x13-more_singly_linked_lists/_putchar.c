32057473228f2684af252880c008c6f2f6266ff2
mrbridge-dev <oseijuantuah@gmail.com>
2022-07-18 06:44:37 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,16 @@
+/**
+ * By: Richard OSei Juantuah
+ */
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
