3fe7858bb385353eb12294713aa333d2ae09e44e
mrbridge-dev <oseijuantuah@gmail.com>
2022-07-18 06:45:55 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+/**
+ * By: Richard Osei Juantuah
+ */
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: The node at the beginning of the list
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		if (head->next)
+			free_listint(head->next);
+		if (head)
+			free(head);
+	}
+}
